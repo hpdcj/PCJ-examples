5d0ad67ac0c8b8f801845c3363621eb4c45a5dcc
Marek Nowicki <faramir2@gmail.com>
2017-05-04 15:07:53 +0200

    Tutorial: step 6: PCJ.get()

---
+++ src/org/pcj/tutorial/Step6_Get.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pcj.tutorial;
+
+import java.util.Random;
+import org.pcj.NodesDescription;
+import org.pcj.PCJ;
+import org.pcj.RegisterStorage;
+import org.pcj.StartPoint;
+import org.pcj.Storage;
+
+/**
+ *
+ * @author faramir
+ */
+@RegisterStorage(Step6_Get.Shared.class)
+public class Step6_Get implements StartPoint {
+
+    @Storage(Step6_Get.class)
+    enum Shared {
+        rand
+    }
+    private double rand;
+
+    @Override
+    public void main() throws Throwable {
+        rand = new Random().nextDouble();
+        for (int i = 0; i < PCJ.threadCount(); i++) {
+            if (PCJ.myId() == i) {
+                System.out.println("Hello from " + PCJ.myId()
+                        + " with value " + rand);
+            }
+            PCJ.barrier();
+        }
+        if (PCJ.myId() == 0) {
+            double sum = 0;
+            for (int i = 0; i < PCJ.threadCount(); i++) {
+                sum += PCJ.<Double>get(i, Shared.rand);
+            }
+            System.out.println("sum = " + sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        PCJ.deploy(Step6_Get.class, new NodesDescription(new String[]{
+            "localhost",
+            "localhost",
+            "localhost:8090",}));
+    }
+}
